4af27535d32a810875c6903937c2f0fb2acfe3a6
hantdev <captainnemot1k60@gmail.com>
2025-04-02 23:22:30 +0700

    Add provisioning and run scripts for Mitras microservices

---
+++ scripts/run.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+###
+# Runs all Mitras microservices (must be previously built and installed).
+#
+# Expects that PostgreSQL and needed messaging DB are alredy running.
+# Additionally, MQTT microservice demands that Redis is up and running.
+#
+###
+
+BUILD_DIR=../build
+
+# Kill all mitras-* stuff
+function cleanup {
+    pkill mitras
+    pkill nats
+}
+
+###
+# NATS
+###
+nats-server &
+counter=1
+until fuser 4222/tcp 1>/dev/null 2>&1;
+do
+    sleep 0.5
+    ((counter++))
+    if [ ${counter} -gt 10 ]
+    then
+        echo "NATS failed to start in 5 sec, exiting"
+        exit 1
+    fi
+    echo "Waiting for NATS server"
+done
+
+###
+# Users
+###
+SMQ_USERS_LOG_LEVEL=info SMQ_USERS_HTTP_PORT=9002 SMQ_USERS_GRPC_PORT=7001 SMQ_USERS_ADMIN_EMAIL=admin@mitras.com SMQ_USERS_ADMIN_PASSWORD=12345678 SMQ_USERS_ADMIN_USERNAME=admin SMQ_EMAIL_TEMPLATE=../docker/templates/users.tmpl $BUILD_DIR/mitras-users &
+
+###
+# Clients
+###
+SMQ_CLIENTS_LOG_LEVEL=info SMQ_CLIENTS_HTTP_PORT=9000 SMQ_CLIENTS_GRPC_PORT=7000 SMQ_CLIENTS_HTTP_PORT=9002 $BUILD_DIR/mitras-clients &
+
+###
+# HTTP
+###
+SMQ_HTTP_ADAPTER_LOG_LEVEL=info SMQ_HTTP_ADAPTER_PORT=8008 SMQ_CLIENTS_GRPC_URL=localhost:7000 $BUILD_DIR/mitras-http &
+
+###
+# WS
+###
+SMQ_WS_ADAPTER_LOG_LEVEL=info SMQ_WS_ADAPTER_HTTP_PORT=8190 SMQ_CLIENTS_GRPC_URL=localhost:7000 $BUILD_DIR/mitras-ws &
+
+###
+# MQTT
+###
+SMQ_MQTT_ADAPTER_LOG_LEVEL=info SMQ_CLIENTS_GRPC_URL=localhost:7000 $BUILD_DIR/mitras-mqtt &
+
+###
+# CoAP
+###
+SMQ_COAP_ADAPTER_LOG_LEVEL=info SMQ_COAP_ADAPTER_PORT=5683 SMQ_CLIENTS_GRPC_URL=localhost:7000 $BUILD_DIR/mitras-coap &
+
+trap cleanup EXIT
+
+while : ; do sleep 1 ; done
