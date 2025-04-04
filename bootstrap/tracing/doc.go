8a46ad1b08adb70a518594036005101525cb2b76
hantdev <captainnemot1k60@gmail.com>
2025-04-04 21:25:06 +0700

    Add tracing middleware for Mitras Users service

---
+++ bootstrap/tracing/doc.go
@@ -0,0 +1,7 @@
+// Package tracing provides tracing instrumentation for Mitras Users service.
+//
+// This package provides tracing middleware for Mitras Users service.
+// It can be used to trace incoming requests and add tracing capabilities to
+// Mitras Users service.
+//
+package tracing
